e14ce66e780d1cef2bf003cdeb9d255b662b695d
kunalvashist <kunal.vash@gmail.com>
2019-05-14 17:22:23 +0530

    TodoApp

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,6 @@
-class ApplicationController < ActionController::Base
+class PagesController < ApplicationController
+
+ def about
+
+ end
 end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,1 +1,55 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :edit, :update, :destroy]
 
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    if @todo.save
+      flash[:notice] = "Todo Created Successfully"
+      redirect_to todo_path(@todo)
+    else
+      render 'new'
+    end
+
+  end
+
+  def show
+    @showtodo = Todo.find(params[:id])
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    if @todo.update(todo_params)
+      flash[:notice] = "Todo Update Successfully"
+      redirect_to todo_path(@todo)
+    else
+      render "edit"
+    end
+  end
+
+  def index
+    @todo = Todo.all
+  end
+
+  def destroy
+    @todo.destroy
+    flash[:notice] = "Todo Deleted Successfully"
+    redirect_to todos_path
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:name, :description)
+  end
+
+end
